0a6cc86828e08bf728c0405761ae3e0a5d70b551
Mr F1d41 <105339452+F1d41@users.noreply.github.com>
2022-05-27 12:09:20 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,70 @@
+clear
+echo ""
+echo ""
+echo ""
+# fpp is the telegram community
+fpp_community_F1d41="\033[1;92m"
+fpp_Community="\033[1;91m"
+fpp_cOmmunity="\033[1;97m"
+fpp_F1d41="\033[0;94m"
+# Function not added
+clear
+echo -e $fpp_cOmmunity
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+echo -e $fpp_cOmmunity "| $fpp_Community   █████╗ █████╗ ██████╗ ████████╗██╗ █████╗ ███╗   ██╗     ██╗  ██╗ $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "| $fpp_Community ██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██║██╔══██╗████╗  ██║     ╚██╗██╔╝ $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "| $fpp_Community ██║     ███████║██████╔╝   ██║   ██║███████║██╔██╗ ██║█████╗╚███╔╝ $fpp_cOmmunity  |"
+echo -e $fpp_cOmmunity "| $fpp_Community ██║     ██╔══██║██╔═══╝    ██║   ██║██╔══██║██║╚██╗██║╚════╝██╔██╗ $fpp_cOmmunity  |"
+echo -e $fpp_cOmmunity "| $fpp_Community ╚██████╗██║  ██║██║        ██║   ██║██║  ██║██║ ╚████║     ██╔╝ ██╗ $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "| $fpp_Community  ╚═════╝╚═╝  ╚═╝╚═╝        ╚═╝   ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝     ╚═╝  ╚═╝ $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+echo -e $fpp_cOmmunity "| $fpp_Community                               Authors                $fpp_cOmmunity                |"
+echo -e $fpp_cOmmunity "| $fpp_F1d41                      https://github.com/RED5PY        $fpp_cOmmunity               |"
+echo -e $fpp_cOmmunity "| $fpp_F1d41                      https://github.com/F1d41         $fpp_cOmmunity               |"
+echo -e $fpp_cOmmunity "|$fpp_community_F1d41 Version 1.0.10  $fpp_cOmmunity                                                      |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+sleep 5
+clear
+# fpp is the telegram community
+read -p " Do you want to watch redspy tech youtube channel [ Y , N ] : " reply
+if [ $reply = y ]; then
+xdg-open https://youtube.com/c/REDSPYTECH
+clear
+elif [ $reply = Y ]; then
+xdg-open https://youtube.com/c/REDSPYTECH
+clear
+else
+echo ""
+clear
+fi
+echo ""
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+echo -e $fpp_cOmmunity "|          $fpp_community_F1d41               DOWNLOAD REQUIREMENTS          $fpp_cOmmunity               |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+sleep 2
+# fpp is the telegram community
+clear
+pkg install python -y
+pkg install python2
+pkg install golang -y
+apt install zip -y
+apt install curl -y
+pkg install wget -y
+chmod 444 *
+clear
+# Function not added
+echo -e $fpp_cOmmunity
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+echo -e $fpp_cOmmunity "| $fpp_Community   █████╗ █████╗ ██████╗ ████████╗██╗ █████╗ ███╗   ██╗     ██╗  ██╗ $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "| $fpp_Community ██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██║██╔══██╗████╗  ██║     ╚██╗██╔╝ $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "| $fpp_Community ██║     ███████║██████╔╝   ██║   ██║███████║██╔██╗ ██║█████╗╚███╔╝ $fpp_cOmmunity  |"
+echo -e $fpp_cOmmunity "| $fpp_Community ██║     ██╔══██║██╔═══╝    ██║   ██║██╔══██║██║╚██╗██║╚════╝██╔██╗ $fpp_cOmmunity  |"
+echo -e $fpp_cOmmunity "| $fpp_Community ╚██████╗██║  ██║██║        ██║   ██║██║  ██║██║ ╚████║     ██╔╝ ██╗ $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "| $fpp_Community  ╚═════╝╚═╝  ╚═╝╚═╝        ╚═╝   ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝     ╚═╝  ╚═╝ $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+echo -e $fpp_cOmmunity "|                       $fpp_community_F1d41 Installation Successfull                      $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+sleep 3
+# copyright : https://github.com/F1d41
+clear
+exit
