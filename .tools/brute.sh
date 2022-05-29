208b9004e9d1959e323132c0926c05b534e8045c
Mr F1d41 <105339452+F1d41@users.noreply.github.com>
2022-05-29 22:46:18 +0530

    Create brute.sh

---
+++ .tools/brute.sh
@@ -0,0 +1,10 @@
+
+
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ██████╗ ██████╗ ██╗   ██╗████████╗███████╗       $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ██╔══██╗██╔══██╗██║   ██║╚══██╔══╝██╔════╝       $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ██████╔╝██████╔╝██║   ██║   ██║   █████╗         $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ██╔══██╗██╔══██╗██║   ██║   ██║   ██╔══╝         $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ██████╔╝██║  ██║╚██████╔╝   ██║   ███████╗       $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ╚═════╝ ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚══════╝       $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
