27ebc62d4321c250fd2e3c8c5a72b579b670d56b
Mr F1d41 <105339452+F1d41@users.noreply.github.com>
2022-05-30 14:14:39 +0530

    Add files via upload

---
+++ .tools/brute.sh
@@ -0,0 +1,135 @@
+fpp_community_F1d41="\033[0;94m"
+fpp_Community="\033[0;32m"
+fpp_cOmmunity="\033[1;97m"
+fpp_F1d41="\033[0;31m"
+fpp_f1d41="\033[0;93m"
+Fpp_community="\033[0;95m"
+clear
+brut () {
+echo -e $fpp_cOmmunity "|                           $fpp_community_F1d41 Brute Force Tools                         $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+}
+choo () {
+# Add Brute Force Tool Name
+echo -e $fpp_f1d41 " 01) #Brute Force                                            #Brute Force"
+echo -e $fpp_f1d41 " 03) #Brute Force                                            #Brute Force"
+echo -e $fpp_f1d41 " 05) #Brute Force                                            #Brute Force"
+echo -e $fpp_f1d41 " 99) Back                                                    00) Exit "
+echo -e $fpp_cOmmunity
+read -p "  What You Want to Choose : " opt
+if [ $opt = 01 ] || [ $opt = 1 ]; then
+echo ""      
+clear
+ban
+down
+echo -e $fpp_cOmmunity " Name $fpp_community_F1d41 : $fpp_f1d41 #Tool name                          $fpp_cOmmunity Created $fpp_community_F1d41 : $fpp_f1d41 # creator name"
+sleep 1
+echo ""
+git clone https://github.com/example
+if [ -d example ]; then
+mv example ~
+clear
+ban
+insta
+echo -e $fpp_f1d41 " 99) Back                                                       00) Exit "
+echo -e $fpp_cOmmunity
+read -p "  What You Want to Choose : " opti
+if [ $opti = 99 ]; then
+ban
+brut
+choo
+elif [ $opti = 00 ]; then
+clear
+ban
+sleep 2
+clear
+ls
+exit
+else
+ban
+inva
+sleep 3
+clear
+ban
+brut
+choo
+fi
+else
+ban
+wrong
+echo " I think maybe your internet is not working"
+sleep 2
+clear
+ban
+brut
+choo
+fi
+elif [ $opt = 02 ] || [ $opt = 2 ]; then
+echo ""
+clear
+ban
+down
+sleep 1
+
+
+elif [ $opt = 03 ] || [ $opt = 3 ]; then
+echo 
+elif [ $opt = 04 ] || [ $opt = 4 ]; then
+echo 
+elif [ $opt = 05 ] || [ $opt = 5 ]; then
+echo 
+elif [ $opt = 06 ] || [ $opt = 6 ]; then
+echo 
+elif [ $opt = 07 ] || [ $opt = 7 ]; then
+echo 
+elif [ $opt = 08 ] || [ $opt = 8 ]; then
+echo 
+elif [ $opt = 99 ]; then
+echo 
+elif [ $opt = 00 ]; then
+echo 
+else
+ban
+inva
+sleep 2
+clear
+ban
+brut
+choo
+fi	
+}
+insta () {
+echo -e $fpp_cOmmunity "|                       $fpp_community_F1d41 Installation Successfull                      $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+}
+# F1d41
+# REDSPY TECH 
+inva () {
+echo -e $fpp_cOmmunity "|                     $fpp_community_F1d41 Invalid option Try Again !!                     $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+}
+ban () {
+clear
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ██████╗ ██████╗ ██╗   ██╗████████╗███████╗       $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ██╔══██╗██╔══██╗██║   ██║╚══██╔══╝██╔════╝       $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ██████╔╝██████╔╝██║   ██║   ██║   █████╗         $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ██╔══██╗██╔══██╗██║   ██║   ██║   ██╔══╝         $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ██████╔╝██║  ██║╚██████╔╝   ██║   ███████╗       $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "|         $fpp_f1d41      ╚═════╝ ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚══════╝       $fpp_cOmmunity       |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+}
+down () {
+echo -e $fpp_cOmmunity "|                           $fpp_community_F1d41 Downloading...                            $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+}
+wrong () {
+echo -e $fpp_cOmmunity "|                          $fpp_community_F1d41 Something wrong                            $fpp_cOmmunity |"
+echo -e $fpp_cOmmunity "+-----------------------------------------------------------------------+"
+}
+option () {
+echo 
+}
+ban
+brut
+choo
